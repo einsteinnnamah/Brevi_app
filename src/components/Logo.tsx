c9c572ec435eb82761a432f0bc40b5a1b21d9625
einsteinnnamah <einsteinnnamah@gmail.com>
2023-06-29 04:07:53 +0100

    uploads

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { useRouter } from "next/navigation";
 
 function Logo() {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
     const router = useRouter()
   return (
    <div className=" lg:text-white text-French-Puce text-[32px] font-bold cursor-pointer">Br<span className="text-Crayola">evi</span></div>

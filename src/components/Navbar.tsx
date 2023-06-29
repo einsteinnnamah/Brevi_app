34dc7696818212cb9b0b3ab1b0785877a8935f80
einsteinnnamah <einsteinnnamah@gmail.com>
2023-06-29 11:46:51 +0100

    added signout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,25 @@
-"use client"
-import React from 'react'
-const Navbar = () => {
-  return (
-    <div className='px-[24px] xl:w-[1440px] mt-[20px] flex justify-between xl:px-[120px] lg:px-[64px] md:px-[32px] m-auto '>
-       <h2>Welcome Kemi 👋</h2>
-       <button>Log out</button>
-    </div>
-  )
-}
+import React from 'react';
+import { auth } from '../pages/firebase';
+import { useRouter } from 'next/router';
 
-export default Navbar
+const Navbar = () => {
+  const router = useRouter();
+
+  const logout = async () => {
+    try {
+      await auth.signOut();
+      router.push('/login');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="px-[24px] xl:w-[1440px] mt-[20px] flex justify-between xl:px-[120px] lg:px-[64px] md:px-[32px] m-auto ">
+      <h2>Welcome Kemi 👋</h2>
+      <button onClick={logout}>Log out</button>
+    </div>
+  );
+};
+
+export default Navbar;
